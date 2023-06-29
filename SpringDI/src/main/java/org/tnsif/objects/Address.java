1e5e6c869d150cce6814409d5253fd59aa113f04
sandhya <sandhya@DESKTOP-4BI2TBA>
2023-06-29 18:01:38 +0530

    first commit

---
+++ SpringDI/src/main/java/org/tnsif/objects/Address.java
@@ -0,0 +1,18 @@
+package org.tnsif.objects;
+
+public class Address {
+	
+	//private data members
+		String city="Nashik";
+		String area="Gangapur road";
+		long pincode=422007;
+		
+		
+		
+		public void display()
+		{
+			System.out.println(city+ " "+area+" "+pincode);
+		}
+
+
+}
---
+++ SpringDI/src/main/java/org/tnsif/objects/Student.java
@@ -0,0 +1,19 @@
+package org.tnsif.objects;
+
+public class Student {
+
+	//DI in the form of object
+	Address address;
+
+	//Achieving DI using constructor
+	public Student(Address address) {
+		super();
+		this.address = address;
+	}
+	
+	public void print()
+	{
+		//we are calling display() method of Address class here
+		address.display();
+	}
+}
